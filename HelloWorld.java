30b9555060b88232a04124a42bbfc13e3ea0a20b
Anis Zarrad (Computer Science) <a.zarrad@bham.ac.uk>
2024-11-12 13:37:31 +0400

    New hellojava

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World 2024!");
+        System.out.println("Hello, World 2025!");
     }
 }
